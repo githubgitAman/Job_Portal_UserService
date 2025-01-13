05c5fb3dd7b389ca6747ae059cdeb3de48dfcca9
githubgitAman <sharma.aman.a30@gmail.com>
2025-01-13 18:54:56 +0530

    Created User Service

---
+++ src/main/java/dev/aman/job_portal_userservice/JobPortalUserServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.aman.job_portal_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aman/job_portal_userservice/JobPortalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.job_portal_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
